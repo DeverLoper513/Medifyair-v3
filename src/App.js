f82d53ac2cd10462d82e0bac11c4fb4a5012d7e2
DeverLoper513 <151453624+DeverLoper513@users.noreply.github.com>
2024-11-22 12:49:19 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ import UgcGallery from "./components/UgcGallery";
 function App() {
   //Instance ID
   let InstanceId_Widget = "664450";
-  let InstanceId_QA = "";
+  let InstanceId_QA = "962565";
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
-  let InstanceId_StarRating = "";
+  let InstanceId_StarRating = "962546";
   let InstanceId_PromotedProd = "";
   let InstanceId_UgcGallery = "";
   let InstanceId_ReviewsTab = "";
